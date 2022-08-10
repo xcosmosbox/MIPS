b88db0f2fb59e8d74b05781eb8148a6d917dfc9f
Yuxiang Feng <yfen0056@student.monash.edu>
2022-08-10 22:30:24 +1000

    Update task1.asm

--- task1.asm
+++ task1.asm
@@ -1,20 +1,3 @@
-# Your job to fill in! :)
-
-#    .data
-
-#fill: .asciiz "You haven't started task 1 yet\n"
-
-#    .text
-
-#la $a0, fill
-#addi $v0, $0, 4
-#syscall
-
-#addi $v0, $0, 10
-#syscall
-
-
-
 .data
 	tier_one_price:	   .word	9
 	tier_two_price:	   .word	11
